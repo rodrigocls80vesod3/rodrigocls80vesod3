0180da8058de3d17822a68a235f342b7bb1efa24
rodrigocls80vesod3 <84034531+rodrigocls80vesod3@users.noreply.github.com>
2021-05-25 13:07:20 -0500

    Update succes.php

--- codeingniter/app/Views/succes.php
+++ codeingniter/app/Views/succes.php
@@ -5,7 +5,8 @@
 <div class="container">
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
-        <br><br> <h2 style="color:#0fad00">Success</h2>
+        <br><br> <h2 style="color:#0fad00"><?php echo $user_name ?></h2>
+             <br><br> <h2 style="color:#0fad00"><?php echo $password ?></h2>
         </div>
     </div>
 </body>
